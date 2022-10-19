3c099a4ca4acd62297cb74544f8096c1444032ab
shiuns <shiunworks@gmail.com>
2022-10-20 02:27:58 +0800

    加入專案檔案。

---
+++ (Exec_StringMembers2_Q8)-21 謝孟勳/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Exec_StringMembers2_Q8__21_謝孟勳
+{
+    //隱藏手機的資訊 呈現 0935****46
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入您的手機號碼 :");
+            string sid = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(sid) || sid.Length != 10)
+            {
+                Console.WriteLine("sid 格式不對");
+                return;
+            }
+
+            string sid2 = sid.Substring(0, 4) + new string('*', 4) + sid.Substring(8, 2);
+
+            Console.WriteLine(sid2);
+        }
+    }
+}
